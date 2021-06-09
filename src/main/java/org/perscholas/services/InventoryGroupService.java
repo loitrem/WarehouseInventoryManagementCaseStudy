702bb2f55a0e7a023f6a0228b4b17d4e71ad93a9
loitrem <ski_fix@hotmail.com>
2021-06-09 16:08:47 -0400

    added inventory group service and permissions service methods

--- src/main/java/org/perscholas/services/InventoryGroupService.java
+++ src/main/java/org/perscholas/services/InventoryGroupService.java
@@ -1,4 +1,42 @@
 package org.perscholas.services;
 
+import org.perscholas.dao.IInventoryGroupRepo;
+import org.perscholas.dao.IInventoryRepo;
+import org.perscholas.models.InventoryGroup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class InventoryGroupService {
+
+    IInventoryGroupRepo iInventoryGroupRepo;
+
+    @Autowired
+    public InventoryGroupService(IInventoryGroupRepo iInventoryGroupRepo) {
+        this.iInventoryGroupRepo = iInventoryGroupRepo;
+    }
+
+    //find all inventory groups
+    public List<InventoryGroup> findAllInventoryGroup(){
+        return iInventoryGroupRepo.findAll();
+    }
+
+    //find inventory group by id
+    public Optional<InventoryGroup> findById(int id){
+        if (id != 0){
+            return iInventoryGroupRepo.findBygId(id);
+        }
+        return Optional.empty();
+    }
+
+    //find inventory group by company name
+    public Optional<InventoryGroup> findByCompanyName(String companyName){
+        if (companyName != null){
+            return iInventoryGroupRepo.findBygCompanyName(companyName);
+        }
+        return Optional.empty();
+    }
 }
--- src/main/java/org/perscholas/services/PermissionsService.java
+++ src/main/java/org/perscholas/services/PermissionsService.java
@@ -1,4 +1,33 @@
 package org.perscholas.services;
 
+import org.perscholas.dao.IPermissionsRepo;
+import org.perscholas.models.Permissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PermissionsService {
+
+    IPermissionsRepo iPermissionsRepo;
+
+    @Autowired
+    public PermissionsService(IPermissionsRepo iPermissionsRepo) {
+        this.iPermissionsRepo = iPermissionsRepo;
+    }
+
+    //find all permissions
+    public List<Permissions> findAllPermissions(){
+        return  iPermissionsRepo.findAll();
+    }
+
+    //find permissions by id
+    public Optional<Permissions> findBypId(int id){
+        if (id != 0){
+            return iPermissionsRepo.findBypId(id);
+        }
+        return Optional.empty();
+    }
 }
