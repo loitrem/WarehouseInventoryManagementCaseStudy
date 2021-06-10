da09c6dde88f86f9d7e02a298808a23ff12e118d
loitrem <ski_fix@hotmail.com>
2021-06-10 10:03:38 -0400

    added user service and userType service methods

--- src/main/java/org/perscholas/services/UserService.java
+++ src/main/java/org/perscholas/services/UserService.java
@@ -1,4 +1,47 @@
 package org.perscholas.services;
 
+import org.perscholas.dao.IUserRepo;
+import org.perscholas.models.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    IUserRepo iUserRepo;
+
+    @Autowired
+    public UserService(IUserRepo iUserRepo) {
+        this.iUserRepo = iUserRepo;
+    }
+
+    //find all users
+    public List<Users> findAllUsers(){ return iUserRepo.findAll(); }
+
+    //find users by id
+    public Optional<Users> findById(int id){
+        if (id != 0){
+            return iUserRepo.findByuId(id);
+        }
+        return Optional.empty();
+    }
+
+    //find users by username
+    public Optional<Users> findByuUsername(String username){
+        if (username != null){
+            return iUserRepo.findByuUsername(username);
+        }
+        return Optional.empty();
+    }
+
+    //find users by usertype
+    public Optional<Users> findByuUserType(int userType){
+        if (userType != 0){
+            return iUserRepo.findByuUserType(userType);
+        }
+        return Optional.empty();
+    }
 }
--- src/main/java/org/perscholas/services/UserTypeService.java
+++ src/main/java/org/perscholas/services/UserTypeService.java
@@ -1,4 +1,31 @@
 package org.perscholas.services;
 
+import org.perscholas.dao.IUserTypeRepo;
+import org.perscholas.models.UserType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserTypeService {
+
+    IUserTypeRepo iUserTypeRepo;
+
+    @Autowired
+    public UserTypeService(IUserTypeRepo iUserTypeRepo) {
+        this.iUserTypeRepo = iUserTypeRepo;
+    }
+
+    //find all user types
+    public List<UserType> findAllUserTypes(){ return iUserTypeRepo.findAll(); }
+
+    //find user types by id
+    public Optional<UserType> findByuserTypeId(int id){
+        if (id != 0){
+            return iUserTypeRepo.findByuserTypeId(id);
+        }
+        return Optional.empty();
+    }
 }
