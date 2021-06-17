dc5a45f9d3110e1c5e56ddfed643d509fe183d5e
loitrem <ski_fix@hotmail.com>
2021-06-17 11:23:05 -0400

    modified index layout,navbar,and added a method to home controller

--- src/main/java/org/perscholas/controllers/HomeController.java
+++ src/main/java/org/perscholas/controllers/HomeController.java
@@ -1,10 +1,26 @@
 package org.perscholas.controllers;
 
+import org.perscholas.models.Employees;
+import org.perscholas.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    EmployeeService employeeService;
+
+    @Autowired
+    public HomeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
     @GetMapping("/template")
     public String template(){
@@ -22,5 +38,12 @@ public class HomeController {
     @GetMapping("/home")
     public String home(){
         return "home";
+    }
+    @GetMapping("/register")
+    public String showEmployees(@ModelAttribute("employees") @Valid Employees employees, BindingResult result, Model model){
+
+        List<Employees> e = employeeService.findAllEmployees();
+        model.addAttribute("employees", e);
+        return "register";
     }
 }
